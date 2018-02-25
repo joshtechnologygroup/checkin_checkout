66496e1a11ae8b46fdab5489d5d9499ae7b843a9
Harshil Sharma <harshilsharma63@gmail.com>
2018-02-25 18:15:28 +0530

    herpderp

---
+++ plugins/attendance.py
@@ -0,0 +1,53 @@
+import re
+import time
+
+from mattermost_bot.bot import respond_to
+
+import sql_driver
+
+
+@respond_to('alert (.*)', re.IGNORECASE)
+def send__ss_alert(message, name):
+    print(name)
+    print(time.strftime('%y-%m-%d %H:%M:%S'))
+
+    request_time = time.strftime('%Y-%m-%d')
+
+    get_user_checking_status_sql = "SELECT top(1) UserId, LogDate, Direction FROM DeviceLogs_2_2018 " \
+                                   "WHERE userid = (SELECT EmployeeCodeInDevice FROM Employees WHERE EmployeeName = '" + name + "')" \
+                                                                                                                                "AND logdate > Convert(datetime, '" + request_time + "') " \
+                                                                                                                                                                                     "ORDER BY logdate DESC;"
+
+    if not verify_employee_exist(name):
+        message.reply('%s does not work here. At least not that I know of. ' % name)
+
+    request_in_progress = True
+    in_progress_message_sent = True
+
+    while request_in_progress:
+        data = sql_driver.execute(get_user_checking_status_sql)
+
+        if len(data) > 0 and data[0] is not None:
+            data = data[0]
+            direction = data[-1]
+            if direction.upper() == 'IN':
+                message.reply('%s is in office' % name)
+                request_in_progress = False
+            elif direction.upper() == 'OUT':
+                message.reply('%s has left' % name)
+                request_in_progress = False
+        else:
+            if in_progress_message_sent:
+                message.reply('%s has not yet arrived in office' % name)
+                in_progress_message_sent = False
+        time.sleep(10)
+
+
+def verify_employee_exist(employeeName):
+    verify_employee_exists_sql = "select EmployeeName FROM Employees " \
+                                 "WHERE EmployeeName = '" + employeeName + "';"
+    check_name_data = sql_driver.execute(verify_employee_exists_sql)
+    if len(check_name_data) > 0 and check_name_data[0] is not None:
+        return True
+    else:
+        return False
--- sql_driver.py
+++ sql_driver.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
 import pyodbc
+
+from retrying import retry
+
 server = '192.168.1.88'
 database = 'SmartOffice'
 username = 'hackathon2'
 password = 'joshlabs'
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect(
+    'DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()
 
 
-def execute(tsql):
+@retry(stop_max_attempt_number=10, wait_random_min=1000, wait_random_max=2000)
+def execute(sql):
     data = []
-    with cursor.execute(tsql):
+    with cursor.execute(sql):
         data.append(cursor.fetchone())
     return data
